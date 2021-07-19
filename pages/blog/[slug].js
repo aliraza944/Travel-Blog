f78eaeea393649a95730f41d2909542da49407da
Ali Raza <alisabri022@gmail.com>
2021-07-19 22:32:41 +0500

    removed the blog/styles file causing deployment errors

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,12 +2,64 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import marked from "marked";
-import useStyles from "./Styles";
 import Image from "next/image";
 import { Typography } from "@material-ui/core";
 import Navigation from "../../Components/Navigation";
 import Footer from "../../Components/Footer";
 import Head from "next/head";
+
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    margin: "100px 0 0 0",
+    fontFamily: "Roboto",
+  },
+  coverimagecontainer: {
+    width: "100%",
+  },
+  title: {
+    fontSize: "2em",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "4em",
+    },
+  },
+
+  blogcontent: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    flexDirection: "column",
+    textAlign: "center",
+  },
+  mdcontent: {
+    width: "100%",
+    textAlign: "center",
+
+    [theme.breakpoints.up("sm")]: {
+      width: "40%",
+      textAlign: "left",
+    },
+
+    "& p": {
+      fontSize: "1em",
+      letterSpacing: "0.08em",
+      lineHeight: 1.8,
+      [theme.breakpoints.up("sm")]: {
+        fontSize: "1.8em",
+      },
+      "& h2": {
+        fontSize: "2em",
+        fontWeight: "300",
+        [theme.breakpoints.up("sm")]: {
+          fontSize: "8em",
+        },
+      },
+    },
+  },
+}));
+
 const BlogPost = ({ frontmatter, slug, content }) => {
   const classes = useStyles();
   return (
